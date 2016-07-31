c7b984e42c88edf2468c839d2d89c413d706d300
Christophe Ecabert <christophe.ecabert@epfl.ch>
2016-08-01 00:02:24 +0200

    ADD: Basic Cocoa OpenGL View

---
+++ modules/ogl/include/chlib/ogl/platform/ogl_view.h
@@ -0,0 +1,15 @@
+/**
+ *  @file   ogl_view.h
+ *  @brief  Custom Cocoa OpenGL view
+ *
+ *  @author Christophe Ecabert
+ *  @date   31/07/16
+ *  Copyright © 2016 Christophe Ecabert. All rights reserved.
+ */
+
+#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
+
+@interface OGLView : NSOpenGLView
+
+@end
