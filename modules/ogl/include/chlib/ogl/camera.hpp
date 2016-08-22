07c0c9230ee45618eab95a2417b425de3134307f
Christophe Ecabert <christophe.ecabert@gmail.com>
2016-08-22 21:11:43 +0200

    ADD: Camera class implementation

--- modules/ogl/include/chlib/ogl/camera.hpp
+++ modules/ogl/include/chlib/ogl/camera.hpp
@@ -1,10 +1,11 @@
 /**
  *  @file   camera.hpp
- *  @brief  OpenGL Camera abstraction class
+ *  @brief  OpenGL Camera abstraction
+ *          Based on : http://learnopengl.com/#!Getting-started/Camera
  *
  *  @author Christophe Ecabert
  *  @date   30/07/16
- *    Copyright © 2016 Christophe Ecabert. All rights reserved.
+ *  Copyright © 2016 Christophe Ecabert. All rights reserved.
  */
 
 #ifndef __CHLIB_OGL_CAMERA__
@@ -12,6 +13,8 @@
 
 #include "chlib/core/library_export.hpp"
 #include "chlib/core/math/vector.hpp"
+#include "chlib/core/math/matrix.hpp"
+#include "chlib/ogl/key_types.hpp"
 
 /**
  *  @namespace  CHLib
@@ -28,6 +31,14 @@ namespace CHLib {
 class CHLIB_EXPORTS OGLCamera {
 
  public:
+  
+#pragma mark -
+#pragma mark Type defintion
+  
+  /** Vector type */
+  using Vec3 = Vector3<float>;
+  /** Matrix type */
+  using Mat4 = Matrix4<float>;
 
 #pragma mark -
 #pragma mark Initialization
@@ -38,6 +49,32 @@ class CHLIB_EXPORTS OGLCamera {
    *  @brief  Constructor
    */
   OGLCamera(void);
+  
+  /**
+   *  @name OGLCamera
+   *  @fn OGLCamera(void)
+   *  @brief  Constructor
+   *  @param[in]  position  Where to place the camera
+   *  @param[in]  target    Where to point at
+   */
+  OGLCamera(const Vec3& position, const Vec3& target);
+  
+  /**
+   *  @name OGLCamera
+   *  @fn OGLCamera(const OGLCamera& other)
+   *  @brief  Copy Constructor
+   *  @param[in]  other Object to copy from
+   */
+  OGLCamera(const OGLCamera& other) = delete;
+  
+  /**
+   *  @name operator=
+   *  @fn OGLCamera& operator=(const OGLCamera& rhs)
+   *  @brief  Assignment operator
+   *  @param[in]  rhs Object to assign from
+   *  @return Newly assigned object
+   */
+  OGLCamera& operator=(const OGLCamera& rhs) = delete;
 
   /**
    *  @name ~OGLCamera
@@ -45,51 +82,116 @@ class CHLIB_EXPORTS OGLCamera {
    *  @brief  Destructor
    */
   ~OGLCamera(void);
+  
+  /**
+   *  @name LookAt
+   *  @fn void LookAt(const Vec3& position, const Vec3& target);
+   *  @brief  Define view transformation
+   *  @param[in]  position  Where to place the camera
+   *  @param[in]  target    Where to point at
+   */
+  void LookAt(const Vec3& position, const Vec3& target);
+  
+#pragma mark -
+#pragma mark Usage
+
+  /**
+   *  @name UpdateProjectionTransform
+   *  @fn void UpdateProjectionTransform(const float fov, 
+                                         const float z_near,
+                                         const float z_far,
+                                         const float aspect);
+   *  @brief  Update projection transform
+   *  @param[in]  fov     Field of view in Y axis
+   *  @param[in]  z_near  New near plane
+   *  @param[in]  z_fat   New far plane
+   *  @param[in]  aspect  Window aspect ratio
+   */
+  void UpdateProjectionTransform(const float fovy,
+                                 const float z_near,
+                                 const float z_far,
+                                 const float aspect);
+  
+  /**
+   *  @name OnKeyboard
+   *  @fn void OnKeyboard(const OGLKey& key, const OGLKeyState& state);
+   *  @brief  Handle keyboard event for camera navigation
+   *  @param[in]  key   Which key trigger the event
+   *  @param[in]  state State of the key (Pressed or released)
+   */
+  void OnKeyboard(const OGLKey& key, const OGLKeyState& state);
 
 #pragma mark -
 #pragma mark Accessors
 
   /**
-   *  @name set_position
-   *  @fn void set_position(const Vector3<float>& position)
-   *  @brief  Set a new camera position
-   *  @param[in]  position  New camera position
+   *  @name get_position
+   *  @fn const Vec3 get_position(void) const
+   *  @brief  Get camera position
    */
-  void set_position(const Vector3<float>& position) {
-    position_ = position;
+  const Vec3 get_position(void) const {
+    return position_;
   }
-
+  
   /**
-   *  @name set_target
-   *  @fn void set_target(const Vector3<float>& target)
-   *  @brief  Set a new target direction
-   *  @param[in]  target  New targeted direction
+   *  @name get_view
+   *  @fn const Mat4 get_view(void) const
+   *  @brief  Get view transformation
    */
-  void set_target(const Vector3<float>& target) {
-    target_ = target;
+  const Mat4 get_view(void) const {
+    return view_;
   }
-
+  
   /**
-   *  @name set_up
-   *  @fn void set_up(const Vector3<float>& up)
-   *  @brief  Set a new up direction
-   *  @param[in]  up  New up direction
+   *  @name get_projection
+   *  @fn const Mat4 get_projection(void) const
+   *  @brief  Get projection transformation
    */
-  void set_up(const Vector3<float>& up) {
-    up_ = up;
+  const Mat4 get_projection(void) const {
+    return projection_;
+  }
+  
+  /**
+   *  @name get_transform
+   *  @fn const Mat4 get_transform(void) const
+   *  @brief  Get full camera transformation
+   */
+  const Mat4 get_transform(void) const {
+    return projection_ * view_;
   }
 
 #pragma mark -
 #pragma mark Private
 
  private:
+  
+  /**
+   *  @name UpdateViewTransform
+   *  @fn void UpdateViewTransform(void);
+   *  @brief  Update view transform
+   */
+  void UpdateViewTransform(void);
 
   /** Camera position */
-  Vector3<float> position_;
+  Vec3 position_;
   /** Traget */
-  Vector3<float> target_;
+  Vec3 target_;
   /** Up direction */
-  Vector3<float> up_;
+  Vec3 up_;
+  /** Right */
+  Vec3 right_;
+  /** Window aspect ratio */
+  float aspect_;
+  /** Near plane */
+  float near_;
+  /** Far plane */
+  float far_;
+  /** Field of view */
+  float fov_;
+  /** View transformation */
+  Mat4 view_;
+  /** Projection transform */
+  Mat4 projection_;
 
 };
 
--- modules/ogl/src/camera.cpp
+++ modules/ogl/src/camera.cpp
@@ -1,14 +1,17 @@
 /**
  *  @file   camera.cpp
- *  @brief
+ *  @brief  OpenGL Camera abstraction
+ *          Based on : http://learnopengl.com/#!Getting-started/Camera
  *
  *  @author Christophe Ecabert
  *  @date   30/07/16
  *  Copyright © 2016 Christophe Ecabert. All rights reserved.
  */
 
-#include "chlib/ogl/camera.hpp"
+#include <limits>
+#include <assert.h>
 
+#include "chlib/ogl/camera.hpp"
 #include "chlib/core/math/vector.hpp"
 #include "chlib/core/math/matrix.hpp"
 
@@ -26,8 +29,36 @@ namespace CHLib {
  *  @fn OGLCamera(void)
  *  @brief  Constructor
  */
-OGLCamera::OGLCamera(void) {
-  Vector2<float> a(1.f, 2.f);
+OGLCamera::OGLCamera(void) :  position_(0.f, 0.f, 1.f),
+target_(0.f, 0.f, 0.f),
+aspect_(1.25f),
+near_(0.01f),
+far_(1000.f),
+fov_(M_PI/6.f) {
+  // Define view
+  this->LookAt(position_, target_);
+  // Define projection
+  this->UpdateProjectionTransform(fov_, near_, far_, aspect_);
+}
+  
+/*
+ *  @name OGLCamera
+ *  @fn OGLCamera(void)
+ *  @brief  Constructor
+ *  @param[in]  position  Where to place the camera
+ *  @param[in]  target    Where to point at
+ */
+OGLCamera::OGLCamera(const Vec3& position, const Vec3& target) :
+position_(position),
+target_(target),
+aspect_(1.25f),
+near_(0.01f),
+far_(1000.f),
+fov_(M_PI/6.f) {
+  // Define view
+  this->LookAt(position_, target_);
+  // Define projection
+  this->UpdateProjectionTransform(fov_, near_, far_, aspect_);
 }
 
 /*
@@ -36,7 +67,92 @@ OGLCamera::OGLCamera(void) {
  *  @brief  Destructor
  */
 OGLCamera::~OGLCamera(void) {
-
+}
+  
+/*
+ *  @name LookAt
+ *  @fn void LookAt(const Vec3& position, const Vec3& target);
+ *  @brief  Define view transformation
+ *  @param[in]  position  Where to place the camera
+ *  @param[in]  target    Where to point at
+ */
+void OGLCamera::LookAt(const Vec3& position, const Vec3& target) {
+  // Define target direction
+  position_ = position;
+  target_ = position_ - target;
+  target_.Normalize();
+  // Define right
+  Vec3 worldUp(0.f, 1.f, 0.f);
+  right_ = target_ ^ worldUp;
+  // Define up vector
+  up_ = target_ ^ up_;
+  // Define view transform
+  this->UpdateViewTransform();
+}
+  
+#pragma mark -
+#pragma mark Usage
+  
+/*
+ *  @name UpdateProjectionTransform
+ *  @fn void UpdateProjectionTransform(const float fov,
+ const float z_near,
+ const float z_far,
+ const float aspect);
+ *  @brief  Update projection transform
+ *  @param[in]  fovy    Field of view in Y axis
+ *  @param[in]  z_near  New near plane
+ *  @param[in]  z_fat   New far plane
+ *  @param[in]  aspect  Window aspect ratio
+ */
+void OGLCamera::UpdateProjectionTransform(const float fovy,
+                                          const float z_near,
+                                          const float z_far,
+                                          const float aspect) {
+  assert(std::abs(aspect - std::numeric_limits<float>::epsilon()) > 0.f);
+  assert(z_near > 0.f && z_far > z_near);
+  aspect_ = aspect;
+  near_ = z_near;
+  far_ = z_far;
+  const float tanHalfFovy = std::tan(fovy / 2.f);
+  projection_[0] = 1.f  / (aspect * tanHalfFovy);
+  projection_[5] = 1.f  / tanHalfFovy;
+  projection_[10] = - (far_ + near_) / (far_ - near_);
+  projection_[14] = -1.f;
+  projection_[11] = - (2.f * far_ * near_) / (far_ - near_);
+  projection_[15] = 0;
+}
+  
+/*
+ *  @name OnKeyboard
+ *  @fn void OnKeyboard(const OGLKey& key, const OGLKeyState& state);
+ *  @brief  Handle keyboard event for camera navigation
+ *  @param[in]  key   Which key trigger the event
+ *  @param[in]  state State of the key (Pressed or released)
+ */
+void OGLCamera::OnKeyboard(const OGLKey& key, const OGLKeyState& state) {
+  
+}
+  
+  
+#pragma mark -
+#pragma mark Private
+  
+/*
+ *  @name UpdateViewTransform
+ *  @fn void UpdateViewTransform(void);
+ *  @brief  Update view transform
+ */
+void OGLCamera::UpdateViewTransform(void) {
+  // Define view transform
+  Mat4 R, T;
+  R[0] = right_.x_; R[4] = right_.y_; R[8] = right_.z_;
+  R[1] = up_.x_; R[5] = up_.y_; R[9] = up_.z_;
+  R[2] = target_.x_; R[6] = target_.y_; R[10] = target_.z_;
+  T[12] = -position_.x_;
+  T[13] = -position_.y_;
+  T[14] = -position_.z_;
+  view_ = R * T;
 }
 
 }  // namespace CHLib
